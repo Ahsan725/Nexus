cd723751c89d8d2c0df7b35a3ff858e337817c39
jawadrada <jawadrada18@gmail.com>
2024-05-13 23:48:41 -0400

    Added Routes

--- Nexus/frontend/client/src/App.jsx
+++ Nexus/frontend/client/src/App.jsx
@@ -1,11 +1,32 @@
-import { useState } from 'react'
 import LandingPage from "./views/LandingPage.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-
   return (
-      <LandingPage />
-  )
+    <>
+      <Router>
+        <div className="App">
+          <NavBar />
+
+          <div>
+            <Routes>
+              <Route path="/" element={< LandingPage />}></Route>
+              {/* <Route path="/about">
+
+              </Route>
+              <Route path="/gallary">
+
+              </Route> */}
+            </Routes>
+          </div>
+
+          <Footer />
+        </div>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
