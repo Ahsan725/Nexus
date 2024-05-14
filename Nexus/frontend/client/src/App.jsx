687ec5fd6dfcb5c52ccc599c5efb09fe45374cd0
jawadrada <jawadrada18@gmail.com>
2024-05-14 18:17:55 -0400

    Added Login and Signup Modals and updated other files

--- Nexus/frontend/client/src/App.jsx
+++ Nexus/frontend/client/src/App.jsx
@@ -1,28 +1,37 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./views/LandingPage.jsx";
-import GalleryViewPage from './views/GalleryViewPage.jsx';
+// import GalleryViewPage from './views/GalleryViewPage';
+import AboutPage from "./views/AboutPage.jsx";
 import NavBar from "./components/NavBar.jsx";
 import Footer from "./components/Footer.jsx";
+import LoginModal from "./views/LoginModal.jsx";
+import SignUpModal from "./views/SignUpModal.jsx";
 
+//Note: Make all routes protected except (Home and About us page)
 function App() {
   return (
     <>
       <Router>
         <div className="App">
-          <NavBar />
-
           <div>
             <Routes>
-              <Route path="/" element={<LandingPage />}></Route>
-              {/* <Route path="/about">
+              {/*Home Route*/}
+              <Route path="/" element={<LandingPage />} />
 
-              </Route> */}
-              <Route path="/gallary" element={<GalleryViewPage />}></Route>
+              {/*About Us Route*/}
+              <Route path="/about" element={<AboutPage />} />
+
+              {/*Spaces Route*/}
+              {/* <Route path="/spaces" element={<GalleryViewPage />} /> */}
+
+              {/*Login Route*/}
+              <Route path="/login" element={<LoginModal />} />
+
+              {/*SignUp Route*/}
+              <Route path="/signup" element={<SignUpModal />} />
             </Routes>
           </div>
-
-          <Footer />
         </div>
       </Router>
     </>
