bac84fd557b7bb7f6f5e116c3fdade9edbca44e4
jawadrada <jawadrada18@gmail.com>
2024-05-15 00:39:10 -0400

    Created previous bookings page

---
+++ Nexus/frontend/client/src/views/MyBookings.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Footer from '../components/Footer';
+import NavBar from '../components/NavBar';
+
+function MyBookings() {
+    return (
+        <>
+            <main>
+                <NavBar />
+                <div>MyBookings</div>
+                <Footer />
+            </main>
+        </>
+    )
+}
+
+export default MyBookings;
+
